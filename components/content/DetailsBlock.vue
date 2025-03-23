2b4bc477da66e48f81005780fb4a55682d2e5839
WenYanLin <clps10070623@gmail.com>
2025-03-23 22:39:10 +0800

    fix: fix: resolve article image display and table responsiveness on narrow screens

--- components/content/DetailsBlock.vue
+++ components/content/DetailsBlock.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="details-block mb-8 text-base-content text-base">
-    <div :class="{ 'btn-primary': isOpen, 'btn-secondary': !isOpen }"
-      class="btn px-4 py-2 flex items-center gap-2 cursor-pointer select-none" @click="toggleDetails">
+    <div class="btn btn-primary px-4 py-2 flex items-center gap-2 cursor-pointer select-none" :data-open="isOpen"
+      @click="toggleDetails">
       <ChevronRight :class="{ 'rotate-90': isOpen }" class="w-6 h-6 transition-transform duration-btn" />
       <span>{{ props.title }}</span>
     </div>
-    <div v-if="isOpen" class="mt-2 p-4 border border-primary rounded-btn">
+    <div v-if="isOpen" class="details-block_content mt-2 p-4 border rounded-btn">
       <slot />
     </div>
   </div>
@@ -27,6 +27,33 @@ const toggleDetails = () => {
 .details-block {
   *:last-child {
     @apply mb-0 text-base;
+  }
+
+  .btn {
+    --tw-bg-opacity: .2;
+    --tw-border-opacity: 0;
+
+    @supports(color: color-mix(in oklab, black, black)) {
+      color: color-mix(in oklch, oklch(var(--primary, var(--base-content))) 90%, oklch(var(--primary-content)));
+    }
+
+    &[data-open=true] {
+      --tw-border-opacity: 1;
+    }
+  }
+
+  &_content {
+    background-color: oklch(var(--primary)/.2);
+    border-color: oklch(var(--primary, var(--base-200)) / var(--tw-border-opacity));
+    --tw-border-opacity: 1;
+
+    @supports(color: color-mix(in oklab, black, black)) {
+      color: color-mix(in oklch, oklch(var(--primary, var(--base-content))) 90%, oklch(var(--primary-content)));
+    }
+
+    @supports not(color: oklch(0% 0 0)) {
+      background-color: var(--fallback-b2);
+    }
   }
 }
 </style>
---
+++ components/content/ProseTable.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="table-container h-fit overflow-x-auto mb-8">
+    <table>
+      <slot />
+    </table>
+  </div>
+</template>
